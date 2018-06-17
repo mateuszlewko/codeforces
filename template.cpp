2b9f1c0181f77150b9990c7fb394301bc3f697aa
Mateusz Lewko <mateusz.lewko@gmail.com>
2018-06-17 14:32:52 +0200

    Added template.

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+
+#define For(i, n) for (int i = 0; i < (n); i++)
+#define ForD(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define SORT(x) sort(begin(x), end(x))
+#define REP(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+using namespace std;
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
+
+// #define SORT(x) sort(begin(x), end(x))
+
+
+int main() {
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    
+}
+
